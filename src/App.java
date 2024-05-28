c7385d04ee579a70623ae934cfe9c732bff83f0b
JonathanGuaman2004 <jonathan.guaman04@epn.edu.ec>
2024-05-28 04:33:29 -0500

    arreglo de errores

--- src/App.java
+++ src/App.java
@@ -1,114 +1,159 @@
 
 import java.util.Scanner;
 
+/**Es el lugar en el que se desarrollan todos los metodos creados por el equipo de trabajo
+ * @author Gualoto.Erick,Gualpa.Nayeli,Guaman.Jonathan,Guanochanga.David,Hernandez.Lizeth,Zambrano.Jhojan
+ * @version 1.0
+ */
 public class App {
-    public static void main(String[] args) throws Exception {
-        //variables
-        Scanner ingresoDatos = new Scanner(System.in);
-        int numRepNum  =0;
-        int numRepCar   =0;
-        int numRepFig   =0;
-        //Declaracion
-        GuamanJonathan gj = new GuamanJonathan();
-        //Operaciones
-        System.out.println("Nombre del Grupo:........................");
-        System.out.println("Gualoto.Erick");
-        System.out.println("Gualpa.Nayeli");
-        System.out.println("Guaman.Jonathan");
-        System.out.println("Guanochanga.David");
-        System.out.println("Hernandez.Lizeth");
-        System.out.println("Zambrano.Jhojan");
-        System.out.println();
-        System.out.println("Indique el numero de repeticiones de las series numéricas");
-        numRepNum=ingresoDatos.nextInt();
-        System.out.println("Indique el numero de repeticiones de las series de caracteres");
-        numRepCar=ingresoDatos.nextInt();
-        System.out.println("Indique el numero de repeticiones de las series de figuras");
-        numRepFig=ingresoDatos.nextInt();
-        //Series numéricas alo
-        System.out.println("Series Numéricas");
-        System.out.println("");
-        System.out.print("SN1: ");
-        gj.jgSN1(numRepNum);
-        System.out.println("");
-        System.out.print("SN2: ");
-        System.out.println("");
-        System.out.print("SN3: ");
-        System.out.println("");
-        System.out.print("SN4: ");
-        System.out.println("");
-        System.out.print("SN5: ");
-        System.out.println("");
-        System.out.print("SN6: ");
-        System.out.println("");
-        System.out.print("SN7: ");
-        System.out.println("");
-        System.out.print("SN8: ");
-        System.out.println("");
-        System.out.print("SN9: ");
-        System.out.println("");
-        System.out.print("SN10: ");
-        System.out.println("");
-        System.out.print("SN12: ");
-        //Series de caracteres
-        System.out.println("Series de caracteres");
-        System.out.println("");
-        System.out.print("SC1: ");
-        System.out.println("");
-        System.out.print("SC2: ");
-        System.out.println("");
-        System.out.print("SC3: ");
-        System.out.println("");
-        System.out.print("SC4: ");
-        System.out.println("");
-        System.out.print("SC5: ");
-        System.out.println("");
-        System.out.print("SC6: ");
-        System.out.println("");
-        System.out.print("SC7: ");
-        System.out.println("");
-        System.out.print("SC8: ");
-        System.out.println("");
-        System.out.print("SC9: ");
-        //Series de Figuras
-        System.out.println("Series de figuras");
-        System.out.println("");
-        System.out.print("F1: ");
-        System.out.println("");
-        System.out.print("F2: ");
-        System.out.println("");
-        System.out.print("F3: ");
-        System.out.println("");
-        System.out.print("F4: ");
-        System.out.println("");
-        System.out.print("F5: ");
-        System.out.println("");
-        System.out.print("F6: ");
-        System.out.println("");
-        System.out.print("F7: ");
-        System.out.println("");
-        System.out.print("F8: ");
-        System.out.println("");
-        System.out.print("F9: ");
-        System.out.println("");
-        System.out.print("F10: ");
-        System.out.println("");
-        System.out.print("F11: ");
-        System.out.println("");
-        System.out.print("F12: ");
-        System.out.println("");
-        System.out.print("F13: ");
-        System.out.println("");
-        System.out.print("F14: ");
-        System.out.println("");
-        System.out.print("F15: ");
-        System.out.println("");
-        System.out.print("F16: ");
-        System.out.println("");
-        System.out.print("F17: ");
-        System.out.println("");
-        System.out.print("F18: ");
-        System.out.println("");
-        System.out.print("F19: ");
-    }
+	public static void main(String[] args) throws Exception {
+		//variables
+		Scanner ingresoDatos = new Scanner(System.in);
+		int numRepNum=0;
+		int numRepCar=0;
+		int numRepFig=0;
+		String tipoCaracter;
+		Boolean banderaRep;
+		//Declaracion
+		GuamanJonathan gj = new GuamanJonathan();
+		//Operaciones
+		System.out.println("Grupo3");
+		System.out.println("Gualoto.Erick");
+		System.out.println("Gualpa.Nayeli");
+		System.out.println("Guaman.Jonathan");
+		System.out.println("Guanochanga.David");
+		System.out.println("Hernandez.Lizeth");
+		System.out.println("Zambrano.Jhojan");
+		System.out.println();
+		System.out.println("|1| Indique el numero de repeticiones de las series numéricas");
+		do {
+			try {
+				numRepNum=Integer.parseInt(ingresoDatos.nextLine());
+				banderaRep=false;
+			} catch (NumberFormatException e){
+				System.out.println("Por favor Ingrese UN NUMERO");
+				banderaRep=true;
+			}
+		}while(banderaRep);
+		System.out.println("|2| Indique el numero de repeticiones de las series de caracteres");
+		do {
+			try {
+				numRepCar=Integer.parseInt(ingresoDatos.nextLine());
+				banderaRep=false;
+			} catch (NumberFormatException e){
+				System.out.println("Por favor Ingrese UN NUMERO");
+				banderaRep=true;
+			}
+		}while(banderaRep);
+		System.out.println("|3| Indique el caracter a usar en las series de caracteres (solo uno)");
+		tipoCaracter=ingresoDatos.nextLine();
+		while(tipoCaracter.length()!=1) {
+			System.out.println("SOLO UNO");
+			tipoCaracter=ingresoDatos.nextLine();
+		}
+		System.out.println("|4| Indique el numero de repeticiones de las series de figuras");
+		do {
+			try {
+				numRepFig=Integer.parseInt(ingresoDatos.nextLine());
+				banderaRep=false;
+			} catch (NumberFormatException e){
+				System.out.println("Por favor Ingrese UN NUMERO");
+				banderaRep=true;
+			}
+		}while(banderaRep);
+		//Series numéricas
+		System.out.println("________________________________________Series Numéricas________________________________________________________________________________");
+		System.out.println();
+		System.out.print("SN1: ");
+		gj.jgSN1(numRepNum);
+		System.out.println();
+		System.out.print("SN2: ");
+		gj.jgSN2(numRepNum);
+		System.out.println();
+		System.out.print("SN3: ");
+		System.out.println();
+		System.out.print("SN4: ");
+		System.out.println();
+		System.out.print("SN5: ");
+		System.out.println();
+		System.out.print("SN6: ");
+		System.out.println();
+		System.out.print("SN7: ");
+		System.out.println();
+		System.out.print("SN8: ");
+		System.out.println();
+		System.out.print("SN9: ");
+		System.out.println();
+		System.out.print("SN10: ");
+		System.out.println();
+		System.out.print("SN12: ");
+		//Series de caracteres
+		System.out.println();
+		System.out.println("________________________________________Series de caracteres________________________________________________________________________________");
+		System.out.println();
+		System.out.print("SC1: ");
+		gj.jgSC1(numRepCar,tipoCaracter);
+		System.out.println();
+		System.out.print("SC2: ");
+		System.out.println();
+		System.out.print("SC3: ");
+		System.out.println();
+		System.out.print("SC4: ");
+		System.out.println();
+		System.out.print("SC5: ");
+		System.out.println();
+		System.out.print("SC6: ");
+		System.out.println();
+		System.out.print("SC7: ");
+		System.out.println();
+		System.out.print("SC8: ");
+		System.out.println();
+		System.out.print("SC9: ");
+		//Series de Figuras
+		System.out.println();
+		System.out.println("________________________________________Series de figuras________________________________________________________________________________");
+		System.out.println();
+		System.out.println("F1:");
+		gj.jgF1(numRepFig);
+		System.out.println();
+		System.out.println("F2:");
+		System.out.println();
+		System.out.println("F3:");
+		System.out.println();
+		System.out.println("F4: ");
+		System.out.println();
+		System.out.println("F5: ");
+		System.out.println();
+		System.out.println("F6: ");
+		System.out.println();
+		System.out.println("F7: ");
+		gj.jgF7(numRepFig);
+		System.out.println();
+		System.out.println("F8: ");
+		System.out.println();
+		System.out.println("F9: ");
+		System.out.println();
+		System.out.println("F10: ");
+		System.out.println();
+		System.out.println("F11: ");
+		System.out.println();
+		System.out.println("F12: ");
+		System.out.println();
+		System.out.println("F13: ");
+		gj.jgF13(numRepFig);
+		System.out.println();
+		System.out.println("F14: ");
+		System.out.println();
+		System.out.println("F15: ");
+		System.out.println();
+		System.out.println("F16: ");
+		System.out.println();
+		System.out.println("F17: ");
+		System.out.println();
+		System.out.println("F18: ");
+		System.out.println();
+		System.out.println("F19: ");
+		gj.jgF19(numRepFig);
+		ingresoDatos.close();
+	}
 }
--- src/GuamanJonathan.java
+++ src/GuamanJonathan.java
@@ -1,23 +1,192 @@
+/**Clase con algunas de las series del PoliReto
+ * @author GuamanJonathan
+ * @version 1.0
+ */
 public class GuamanJonathan {
-    public static void jgSN1(int jgTamano) {
-        if (jgTamano<0){
-            System.out.println();
-            System.out.println();
-        }else if (jgTamano<2) {
-            System.out.println("0 ");
-            System.out.println(); 
-        }else{
-            int jgPrim=0;
-            int jgSeg=1;
-            System.out.print("0 ");
-            System.out.print("1 ");
-            for(int jgRep=0;jgRep<jgTamano-2;jgRep++){
-                int suma=jgPrim+jgSeg;
-                System.out.print(suma+" ");
-                jgPrim=jgSeg;
-                jgSeg=suma;
-            }
-            System.out.println("");
-        }
-    }
+	/**crea una serie de acuerdo al tamano solicitado
+	 * @author GuamanJonathan
+	 * @param jgTamano: escribe el tamano de la serie
+	 */
+	public void jgSN1(int jgTamano) {
+		if (jgTamano<=0){
+			System.out.println();
+		}else if (jgTamano<2) {
+			System.out.println("0 ");
+		}else{
+			int jgPrim=0;
+			int jgSeg=1;
+			System.out.print("0 ");
+			System.out.print("1 ");
+			for(int jgRep=0;jgRep<jgTamano-2;jgRep++){
+				int suma=jgPrim+jgSeg;
+				System.out.print(suma+" ");
+				jgPrim=jgSeg;
+				jgSeg=suma;
+			}
+			System.out.println();
+		}
+	}
+	/**crea una serie de acuerdo al tamano solicitado
+	 * @author GuamanJonathan
+	 * @param jgTamano: escribe el tamano de la serie
+	 */
+	public void jgSN2(int jgTamano) {
+		if (jgTamano<=0){
+			System.out.println();
+		}else if (jgTamano<2) {
+			System.out.println("1 ");
+		}else{
+			for(int jgLug=1;jgLug<jgTamano+1;jgLug++) {
+				if((jgLug%2)==0) {
+					System.out.print("0 ");
+				}else {
+					System.out.print(jgLug+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	/**crea una serie de acuerdo al tamano y caracter solicitado
+	 * @author GuamanJonathan
+	 * @param jgTamano: escribe el tamano de la serie
+	 * @param tipoCaracter: escribe el carcter que aparezca en la serie
+	 */
+	public void jgSC1(int jgTamano,String tipoCaracter) {
+		if (jgTamano<0){
+			System.out.println();
+		}else if (jgTamano<2) {
+			System.out.println(tipoCaracter+" ");
+		}else{
+			for(int jgLug=0;jgLug<jgTamano;jgLug++) {
+				if((jgLug%2)==0) {
+					System.out.print(tipoCaracter+" ");
+				}else {
+					System.out.print("- ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	/**crea una figura de acuerdo al tamano solicitado
+	 * @author GuamanJonathan
+	 * @param jgTamano: escribe el tamano de la figura
+	 */
+	public void jgF1(int jgTamano) {
+		if (jgTamano<=0){
+			System.out.println();
+		}else if (jgTamano==1) {
+			System.out.println("* ");
+		}else if(jgTamano==2){
+			System.out.println("* *");
+			System.out.println("* *");
+		}else {
+			for(int jgLug=0;jgLug<jgTamano;jgLug++) {
+				if(jgLug==0||jgLug==jgTamano-1) {
+					for(int jgPos=0;jgPos<jgTamano;jgPos++) {
+						System.out.print("* ");
+					}
+					System.out.println();
+				}else {
+					System.out.print("* ");
+					for(int jgPos=0;jgPos<jgTamano-2;jgPos++) {
+						System.out.print("  ");
+					}
+					System.out.println("* ");
+				}
+			}
+		}
+	}
+	/**crea una figura de acuerdo al tamano solicitado
+	 * @author GuamanJonathan
+	 * @param jgTamano: escribe el tamano de la figura
+	 */
+	public void jgF7(int jgTamano) {
+		if (jgTamano<=0){
+			System.out.println();
+		}else if (jgTamano==1) {
+			System.out.println(" _ _ _ ");
+		}else if(jgTamano==2){
+			System.out.println(" _ _ _");
+			System.out.println("      |_ _ _");
+		}else {
+			for(int jgLug=0;jgLug<jgTamano;jgLug++) {
+				for (int jgPos=0;jgPos<jgTamano;jgPos++) {
+					if((jgLug==0)&&(jgPos==0)) {
+						System.out.print(" _ _ _ ");
+					}else if(jgLug==jgPos) {
+						System.out.print("|_ _ _");
+					}else {
+						System.out.print("      ");
+					}
+				}
+				System.out.println();
+			}
+		}
+	}
+	/**crea una figura de acuerdo al tamano solicitado
+	 * @author GuamanJonathan
+	 * @param jgTamano: escribe el tamano de la figura
+	 */
+	public void jgF13(int jgTamano) {
+		if (jgTamano<=0){
+			System.out.println();
+		}else if (jgTamano==1) {
+			System.out.println("1");
+		}else {
+			for(int jgLug=1;jgLug<jgTamano+1;jgLug++) {
+				for (int jgPos=1;jgPos<jgLug+1;jgPos++) {
+					System.out.print(jgPos+" ");
+				}
+				System.out.println();
+			}
+		}
+	}
+	/**crea una figura de acuerdo al tamano solicitado
+	 * @author GuamanJonathan
+	 * @param jgTamanoInit: escribe el tamano de la figura
+	 */
+	public void jgF19(int jgTamanoInit) {
+		if (jgTamanoInit<=0){
+			System.out.println();
+		}else if (jgTamanoInit==1) {
+			System.out.println("* ");
+		}else if(jgTamanoInit==2){
+			System.out.println("* ");
+			System.out.println("* +");
+		}else {
+			System.out.println("* ");
+			System.out.println("* +");
+			int jgTamano=jgTamanoInit-2;
+			int [][] jgArreglo = new int [jgTamano][jgTamano];
+			for(int jgColum = 0;jgColum<jgTamano;jgColum++) {
+				jgArreglo[jgColum][0]=jgColum+3;
+			}
+			int jgInic=3;
+			for(int jgColum = 0;jgColum<jgTamano;jgColum++) {
+				jgArreglo[jgColum][jgColum]=jgInic;
+				jgInic=jgInic+2;
+			}
+			if(jgArreglo[0].length>2) {
+				int jgFila=1;
+				for(int jgPlace=2;jgPlace<jgArreglo[0].length;jgPlace++) {
+					for(int jgPol=0;jgPol<jgFila;jgPol++) {
+						jgArreglo[jgPlace][jgPol+1]=jgArreglo[jgPlace-1][jgPol]+jgArreglo[jgPlace-1][jgPol+1];
+					}
+					jgFila++;
+				}
+			}
+			for(int jgFilas=0;jgFilas<jgArreglo[0].length;jgFilas++) {
+				System.out.print("* ");
+				for(int jgColumnas=0;jgColumnas<jgArreglo[0].length;jgColumnas++) {
+					if(jgArreglo[jgFilas][jgColumnas]==0) {
+						System.out.print("");
+					}else {
+						System.out.print(jgArreglo[jgFilas][jgColumnas]+" ");
+					}
+				}
+				System.out.print("+");
+				System.out.println();
+			}
+		}
+	}
 }
