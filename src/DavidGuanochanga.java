ac5c2cd40604343a8dc63580686916a153137665
“Divad-exe“ <david.guanochanga@epn.edu.ec>
2024-05-28 18:58:48 +0100

    Mi parte de los poliretos

---
+++ src/DavidGuanochanga.java
@@ -0,0 +1,149 @@
+public class DavidGuanochanga {
+
+    public void dgSN1(int dgnmrTerminos) {
+        if (dgnmrTerminos > 0) {
+            int dgtermino1 = 0;
+            int dgtermino2 = 1;
+
+            if (dgnmrTerminos >= 1) {
+                System.out.print(dgtermino1);
+            }
+            if (dgnmrTerminos >= 2) {
+                System.out.print(" " + dgtermino2);
+            }
+
+            int dgterminoSiguiente;
+            for (int i = 3; i <= dgnmrTerminos; ++i) {
+                dgterminoSiguiente = dgtermino1 + dgtermino2;
+                System.out.print(" " + dgterminoSiguiente);
+
+                dgtermino1 = dgtermino2;
+                dgtermino2 = dgterminoSiguiente;
+            }
+        } else {
+            System.out.println("El numero de terminos debe ser mayor que 0 !!!");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public void dgSN6(int dgnmrTerminos) {
+        if (dgnmrTerminos > 0) {
+            for (int i = 1; i <= dgnmrTerminos; i++) {
+                int dgcuadrado = i * i;
+                System.out.print(dgcuadrado);
+                if (i < dgnmrTerminos) {
+                    System.out.print(" ");
+                }
+            }
+        } else {
+            System.out.println("El numero de terminos debe ser mayor que 0 !!!");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public void dgSC5(int dgnmrTerminos) {
+        if (dgnmrTerminos > 0) {
+            String[] patron = { "\\", "|", "/", " -", " | " };
+
+            for (int i = 0; i < dgnmrTerminos; i++) {
+                System.out.print(patron[i % 5] + " ");
+            }
+        } else {
+            System.out.println("El numero de terminos debe ser mayor que 0 !!!");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public void dgSC8(int dgnmrTerminos) {
+        if (dgnmrTerminos > 0) {
+            char dgletra = 'a';
+            int dgrepeticiones = 1;
+            for (int i = 0; i < dgnmrTerminos; i++) {
+                for (int j = 0; j < dgrepeticiones; j++) {
+                    System.out.print(dgletra);
+                }
+                if (i < dgnmrTerminos - 1) {
+                    System.out.print(" ");
+                }
+                dgletra++;
+                dgrepeticiones += 2;
+            }
+        } else {
+            System.out.println("El numero de terminos debe ser mayor que 0 !!!");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public void dgSF5(int dgnmrNiveles) {
+        if (dgnmrNiveles > 0) {
+            int dgfilas;
+            int dgnroCaracteres;
+            int dgespacio;
+            for (dgfilas = dgnmrNiveles; dgfilas > 0; dgfilas--) {
+                for (dgespacio = dgnmrNiveles; dgespacio > dgfilas; dgespacio--) {
+                    System.out.print(" ");
+                }
+                for (dgnroCaracteres = 0; dgnroCaracteres < dgfilas; dgnroCaracteres++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("El numero de niveles debe ser mayor que 0 !!!");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public void dgSF11(int dgnmrNiveles) {
+        if (dgnmrNiveles > 0) {
+            for (int nivel = 1; nivel <= dgnmrNiveles; nivel++) {
+                for (int espacios = 0; espacios < nivel * nivel - 1; espacios++)
+                    System.out.print(" ");
+                System.out.print("|");
+                for (int base = 0; base < nivel; base++)
+                    System.out.print(" _");
+                System.out.println();
+            }
+            System.out.println();
+        } else {
+            System.out.println("El numero de niveles debe ser mayor que 0 !!!");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    public void dgSF17(int dgnmrNiveles) {
+        if (dgnmrNiveles > 0) {
+            int a = 1;
+
+            for (int i = 1; i <= dgnmrNiveles; i++) {
+                char dgsigno;
+
+                if (a % 2 == 0) {
+                    dgsigno = '0';
+                } else {
+                    dgsigno = '1';
+                }
+                for (int j = 1; j <= dgnmrNiveles; j++) {
+                    if (j == i || j == (dgnmrNiveles - i + 1)) {
+                        System.out.print(dgsigno + " ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+                a++;
+            }
+            System.out.println();
+        } else {
+            System.out.println("El numero de niveles debe ser mayor que 0 !!!");
+        }
+        System.out.println();
+        System.out.println();
+    }
+}
